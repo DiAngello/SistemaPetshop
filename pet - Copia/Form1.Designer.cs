152655a573de58c9a2c213cf775ec0ed190e4973
Andressa de Souza <84677438+DiAngello@users.noreply.github.com>
2022-09-19 03:45:11 -0300

    Add files via upload

---
+++ pet - Copia/Form1.Designer.cs
@@ -0,0 +1,214 @@
+﻿namespace pet
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.PanelLateral = new System.Windows.Forms.Panel();
+            this.PanelCentral = new System.Windows.Forms.Panel();
+            this.PanelTopo = new System.Windows.Forms.Panel();
+            this.Cadastro = new System.Windows.Forms.Button();
+            this.Atendimento = new System.Windows.Forms.Button();
+            this.Financeiro = new System.Windows.Forms.Button();
+            this.Inicio = new System.Windows.Forms.Button();
+            this.PanelSelect = new System.Windows.Forms.Panel();
+            this.btnMin = new System.Windows.Forms.Button();
+            this.btnSair = new System.Windows.Forms.Button();
+            this.Carrossel1 = new System.Windows.Forms.Panel();
+            this.PanelLateral.SuspendLayout();
+            this.PanelTopo.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // PanelLateral
+            // 
+            this.PanelLateral.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.PanelLateral.Controls.Add(this.Carrossel1);
+            this.PanelLateral.Controls.Add(this.PanelSelect);
+            this.PanelLateral.Controls.Add(this.Inicio);
+            this.PanelLateral.Controls.Add(this.Financeiro);
+            this.PanelLateral.Controls.Add(this.Atendimento);
+            this.PanelLateral.Controls.Add(this.Cadastro);
+            this.PanelLateral.Location = new System.Drawing.Point(0, 55);
+            this.PanelLateral.Name = "PanelLateral";
+            this.PanelLateral.Size = new System.Drawing.Size(288, 738);
+            this.PanelLateral.TabIndex = 0;
+            this.PanelLateral.Paint += new System.Windows.Forms.PaintEventHandler(this.PanelLateral_Paint);
+            // 
+            // PanelCentral
+            // 
+            this.PanelCentral.BackColor = System.Drawing.Color.PeachPuff;
+            this.PanelCentral.Location = new System.Drawing.Point(287, 58);
+            this.PanelCentral.Name = "PanelCentral";
+            this.PanelCentral.Size = new System.Drawing.Size(1132, 735);
+            this.PanelCentral.TabIndex = 1;
+            // 
+            // PanelTopo
+            // 
+            this.PanelTopo.BackColor = System.Drawing.Color.LightCoral;
+            this.PanelTopo.Controls.Add(this.btnMin);
+            this.PanelTopo.Controls.Add(this.btnSair);
+            this.PanelTopo.Dock = System.Windows.Forms.DockStyle.Top;
+            this.PanelTopo.Location = new System.Drawing.Point(0, 0);
+            this.PanelTopo.Name = "PanelTopo";
+            this.PanelTopo.Size = new System.Drawing.Size(1419, 57);
+            this.PanelTopo.TabIndex = 2;
+            // 
+            // Cadastro
+            // 
+            this.Cadastro.BackColor = System.Drawing.SystemColors.ButtonFace;
+            this.Cadastro.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.Cadastro.Location = new System.Drawing.Point(46, 281);
+            this.Cadastro.Name = "Cadastro";
+            this.Cadastro.Size = new System.Drawing.Size(195, 68);
+            this.Cadastro.TabIndex = 0;
+            this.Cadastro.Text = "Cadastro Pet";
+            this.Cadastro.UseVisualStyleBackColor = false;
+            this.Cadastro.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Atendimento
+            // 
+            this.Atendimento.BackColor = System.Drawing.SystemColors.ButtonFace;
+            this.Atendimento.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.Atendimento.Location = new System.Drawing.Point(46, 376);
+            this.Atendimento.Name = "Atendimento";
+            this.Atendimento.Size = new System.Drawing.Size(195, 68);
+            this.Atendimento.TabIndex = 1;
+            this.Atendimento.Text = "Atendimentos";
+            this.Atendimento.UseVisualStyleBackColor = false;
+            this.Atendimento.Click += new System.EventHandler(this.Atendimento_Click);
+            // 
+            // Financeiro
+            // 
+            this.Financeiro.BackColor = System.Drawing.SystemColors.ButtonFace;
+            this.Financeiro.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.Financeiro.Location = new System.Drawing.Point(46, 467);
+            this.Financeiro.Name = "Financeiro";
+            this.Financeiro.Size = new System.Drawing.Size(195, 68);
+            this.Financeiro.TabIndex = 2;
+            this.Financeiro.Text = "Financeiro";
+            this.Financeiro.UseVisualStyleBackColor = false;
+            this.Financeiro.Click += new System.EventHandler(this.Financeiro_Click);
+            // 
+            // Inicio
+            // 
+            this.Inicio.BackColor = System.Drawing.SystemColors.ButtonFace;
+            this.Inicio.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.Inicio.Location = new System.Drawing.Point(46, 183);
+            this.Inicio.Name = "Inicio";
+            this.Inicio.Size = new System.Drawing.Size(195, 68);
+            this.Inicio.TabIndex = 3;
+            this.Inicio.Text = "Inicio";
+            this.Inicio.UseVisualStyleBackColor = false;
+            this.Inicio.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // PanelSelect
+            // 
+            this.PanelSelect.BackColor = System.Drawing.SystemColors.MenuHighlight;
+            this.PanelSelect.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.PanelSelect.Location = new System.Drawing.Point(27, 183);
+            this.PanelSelect.Name = "PanelSelect";
+            this.PanelSelect.Size = new System.Drawing.Size(13, 68);
+            this.PanelSelect.TabIndex = 0;
+            // 
+            // btnMin
+            // 
+            this.btnMin.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnMin.BackColor = System.Drawing.Color.LightCoral;
+            this.btnMin.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("btnMin.BackgroundImage")));
+            this.btnMin.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.btnMin.FlatAppearance.BorderSize = 0;
+            this.btnMin.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnMin.Location = new System.Drawing.Point(1311, 9);
+            this.btnMin.Margin = new System.Windows.Forms.Padding(0);
+            this.btnMin.Name = "btnMin";
+            this.btnMin.Size = new System.Drawing.Size(39, 36);
+            this.btnMin.TabIndex = 1;
+            this.btnMin.UseVisualStyleBackColor = false;
+            this.btnMin.Click += new System.EventHandler(this.btnMin_Click);
+            // 
+            // btnSair
+            // 
+            this.btnSair.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnSair.BackColor = System.Drawing.Color.LightCoral;
+            this.btnSair.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("btnSair.BackgroundImage")));
+            this.btnSair.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.btnSair.FlatAppearance.BorderSize = 0;
+            this.btnSair.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnSair.Location = new System.Drawing.Point(1368, 9);
+            this.btnSair.Margin = new System.Windows.Forms.Padding(0);
+            this.btnSair.Name = "btnSair";
+            this.btnSair.Size = new System.Drawing.Size(42, 36);
+            this.btnSair.TabIndex = 0;
+            this.btnSair.UseVisualStyleBackColor = false;
+            this.btnSair.Click += new System.EventHandler(this.button1_Click_1);
+            // 
+            // Carrossel1
+            // 
+            this.Carrossel1.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("Carrossel1.BackgroundImage")));
+            this.Carrossel1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.Carrossel1.Location = new System.Drawing.Point(46, 23);
+            this.Carrossel1.Name = "Carrossel1";
+            this.Carrossel1.Size = new System.Drawing.Size(195, 138);
+            this.Carrossel1.TabIndex = 0;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 27F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1419, 792);
+            this.Controls.Add(this.PanelCentral);
+            this.Controls.Add(this.PanelLateral);
+            this.Controls.Add(this.PanelTopo);
+            this.Font = new System.Drawing.Font("Cascadia Mono", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.PanelLateral.ResumeLayout(false);
+            this.PanelTopo.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel PanelLateral;
+        private System.Windows.Forms.Button Inicio;
+        private System.Windows.Forms.Button Financeiro;
+        private System.Windows.Forms.Button Atendimento;
+        private System.Windows.Forms.Button Cadastro;
+        private System.Windows.Forms.Panel PanelCentral;
+        private System.Windows.Forms.Panel PanelTopo;
+        private System.Windows.Forms.Button btnSair;
+        private System.Windows.Forms.Button btnMin;
+        private System.Windows.Forms.Panel PanelSelect;
+        private System.Windows.Forms.Panel Carrossel1;
+    }
+}
+
---
+++ pet - Copia/FormCadastro.Designer.cs
@@ -0,0 +1,138 @@
+﻿namespace pet
+{
+    partial class FormCadastro
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.PanelCadPet = new System.Windows.Forms.Panel();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.Dados = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.PanelCadPet.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // PanelCadPet
+            // 
+            this.PanelCadPet.BackColor = System.Drawing.Color.LightCoral;
+            this.PanelCadPet.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            this.PanelCadPet.Controls.Add(this.richTextBox1);
+            this.PanelCadPet.Dock = System.Windows.Forms.DockStyle.Top;
+            this.PanelCadPet.Location = new System.Drawing.Point(0, 0);
+            this.PanelCadPet.Name = "PanelCadPet";
+            this.PanelCadPet.Size = new System.Drawing.Size(1132, 88);
+            this.PanelCadPet.TabIndex = 0;
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.BackColor = System.Drawing.Color.LightCoral;
+            this.richTextBox1.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.richTextBox1.Font = new System.Drawing.Font("Cascadia Mono", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.richTextBox1.ForeColor = System.Drawing.SystemColors.InfoText;
+            this.richTextBox1.Location = new System.Drawing.Point(12, 28);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(310, 57);
+            this.richTextBox1.TabIndex = 1;
+            this.richTextBox1.Text = "Cadastro do animal";
+            this.richTextBox1.TextChanged += new System.EventHandler(this.richTextBox1_TextChanged);
+            // 
+            // Dados
+            // 
+            this.Dados.BackColor = System.Drawing.Color.Gainsboro;
+            this.Dados.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.Dados.Font = new System.Drawing.Font("Cascadia Mono", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Dados.Location = new System.Drawing.Point(41, 141);
+            this.Dados.Name = "Dados";
+            this.Dados.Size = new System.Drawing.Size(120, 54);
+            this.Dados.TabIndex = 1;
+            this.Dados.Text = "Dados";
+            this.Dados.UseVisualStyleBackColor = false;
+            this.Dados.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.Gainsboro;
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button1.Font = new System.Drawing.Font("Cascadia Mono", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(293, 141);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(120, 54);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = false;
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.Color.Gainsboro;
+            this.button2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button2.Font = new System.Drawing.Font("Cascadia Mono", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.Location = new System.Drawing.Point(167, 141);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(120, 54);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "button2";
+            this.button2.UseVisualStyleBackColor = false;
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.Gainsboro;
+            this.panel1.Font = new System.Drawing.Font("Cascadia Mono", 7.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.panel1.Location = new System.Drawing.Point(41, 193);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(1033, 442);
+            this.panel1.TabIndex = 4;
+            // 
+            // FormCadastro
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.MistyRose;
+            this.ClientSize = new System.Drawing.Size(1132, 735);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.Dados);
+            this.Controls.Add(this.PanelCadPet);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "FormCadastro";
+            this.Text = "FormCadastro";
+            this.PanelCadPet.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel PanelCadPet;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.Button Dados;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Panel panel1;
+    }
+}
